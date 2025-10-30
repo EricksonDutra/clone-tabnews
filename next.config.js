bad5f0825d0e2ea0ed8702c8fee598a3238690f8
Erickson Dutra <63134236+EricksonDutra@users.noreply.github.com>
2025-10-30 15:41:03 +0000

    pronto para primeiro deploy

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['st.depositphotos.com', 'png.pngtree.com'],
+  },
+};
+
+module.exports = nextConfig;
---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import { useState } from "react";
+
+function Home() {
+  const [reacao, setReacao] = useState(false);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-red-500 text-white">
+      <h1 className="text-2xl font-bold mb-4">Gostou do site?</h1>
+
+      <div className="flex gap-8">
+        <Image
+          src="https://st.depositphotos.com/1001911/55508/v/450/depositphotos_555089554-stock-illustration-happy-emoji-emoticon-showing-double.jpg"
+          width={200}
+          height={200}
+          alt="Emoji feliz"
+          onClick={() => setReacao(true)}
+          className="cursor-pointer hover:scale-105 transition-transform"
+        />
+
+        <Image
+          src="https://png.pngtree.com/png-clipart/20240826/original/pngtree-clipart-of-sad-emoji-png-image_15854157.png"
+          width={200}
+          height={200}
+          alt="Outro emoji feliz"
+          onClick={() => setReacao(false)}
+          className="cursor-pointer hover:scale-105 transition-transform "
+        />
+      </div>
+
+      <h1 className="mt-6 text-xl">
+        {reacao ? "😄 Que bom que gostou!" : "🤔 Ainda pensando..."}
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
