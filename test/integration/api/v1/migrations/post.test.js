6e1b2a0fb71bcff136dd7e66f707e3598a7caf3c
Erickson Dutra <63134236+EricksonDutra@users.noreply.github.com>
2026-01-16 15:26:59 +0000

    refactor: use `orchestrator.cleanDatabase()` in tests

--- test/integration/api/v1/migrations/post.test.js
+++ test/integration/api/v1/migrations/post.test.js
@@ -1,11 +1,10 @@
-import database from "infra/database";
 import orchestrator from "../orchestrator";
 
 beforeAll(cleanDatabase);
 
 async function cleanDatabase() {
   await orchestrator.waitForAllServices();
-  await database.query("drop schema public cascade; create schema public;");
+    await orchestrator.clearDatabase();
 }
 
 describe("POST /api/v1/migrations", () => {
