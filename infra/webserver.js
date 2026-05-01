88388a63d6868e708cf1fea6c8bd12433abea2f6
Erickson Dutra <63134236+EricksonDutra@users.noreply.github.com>
2026-05-01 23:57:57 +0000

    feat: send activation email after `user` registration

---
+++ infra/webserver.js
@@ -0,0 +1,16 @@
+function getOrigin() {
+  if (["test", "development"].includes(process.env.NODE_ENV)) {
+    return "http://localhost:3000";
+  }
+
+  if (process.env.VERCE_ENV === "preview") {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+  return "https://ericksdutra.com.br/";
+}
+
+const webserver = {
+  origin: getOrigin(),
+};
+
+export default webserver;
