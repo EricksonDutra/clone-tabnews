472ac06f50742362156e15bb43f7c3b12e821c92
Erickson Dutra <63134236+EricksonDutra@users.noreply.github.com>
2026-05-15 12:29:36 +0000

    feat: require `read:session` to access `/user` endpoint

--- models/activation.js
+++ models/activation.js
@@ -114,7 +114,6 @@ async function activateUserByUserId(userId) {
   const activatedUser = await user.setFeatures(userId, [
     "create:session",
     "read:session",
-    "update:user",
   ]);
   return activatedUser;
 }
